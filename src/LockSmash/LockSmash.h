39450f31088657ce71adca3ec5db44f0a5e56060
Karl Edenstål <karl.edenstal@gmail.com>
2023-01-30 18:34:32 +0100

    some changes

--- src/LockSmash/LockSmash.h
+++ src/LockSmash/LockSmash.h
@@ -14,10 +14,49 @@ public:
     }
 
     RE::BSEventNotifyControl ProcessEvent(const RE::TESHitEvent* event, RE::BSTEventSource<RE::TESHitEvent>*) {
-        auto targetName = event->target->GetBaseObject()->GetName();
-        auto sourceName = event->cause->GetBaseObject()->GetName();
-        logger::info("{} hit {}", sourceName, targetName);
-        if (event->flags.any(RE::TESHitEvent::Flag::kPowerAttack)) logger::info("Ooooo power attack!");
+        auto lockLevel = event->target->GetLockLevel();
+        auto* player = RE::TESForm::LookupByID<RE::PlayerCharacter>(0x14);
+
+        auto* leftHandedEquip = player->GetEquippedObject(true);
+        auto* rightHandedEquip = player->GetEquippedObject(false); 
+
+        if (leftHandedEquip) {
+            logger::info("Player left hand: {}", leftHandedEquip->GetName());
+        }
+
+        if (rightHandedEquip) {
+            logger::info("Player right hand: {}", rightHandedEquip->GetName());
+        }
+
+        switch (lockLevel) {
+            case RE::LOCK_LEVEL::kRequiresKey: {
+                logger::info("This lock requires a key");
+                break;
+            }
+            case RE::LOCK_LEVEL::kVeryEasy: {
+                logger::info("Novice lock");
+                break;
+            }
+            case RE::LOCK_LEVEL::kEasy: {
+                logger::info("Apprentice lock");
+                break;
+            }
+            case RE::LOCK_LEVEL::kAverage: {
+                logger::info("Adept lock");
+                break;
+            }
+            case RE::LOCK_LEVEL::kHard: {
+                logger::info("Expert lock");
+                break;
+            }
+            case RE::LOCK_LEVEL::kVeryHard: {
+                logger::info("Master lock");
+                break;
+            }
+            default:
+                logger::info("Not even locked, dude");
+        }
+
         return RE::BSEventNotifyControl::kContinue;
     }
 };
